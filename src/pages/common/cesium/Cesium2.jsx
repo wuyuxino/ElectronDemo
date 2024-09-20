df337276c9a781ee9697670abe4fc90a9dc38a4d
wyx <51903@qq.com>
2024-09-20 20:38:11 +0800

    feat: add load gltf

--- src/pages/common/cesium/Cesium2.jsx
+++ src/pages/common/cesium/Cesium2.jsx
@@ -5,9 +5,19 @@ export default function Cesium2() {
     var viewer
 
     async function drawCesium() {
-        viewer = new Cesium.Viewer("cesium2", {
 
-        });
+
+
+        viewer = new Cesium.Viewer("cesium2", {});
+
+        // 模型的URL
+        var modelUrl = "/scene/gltf/girl/girl.gltf";
+
+        console.log(Cesium.VERSION)
+
+        console.log(modelUrl)
+
+
     }
 
     useEffect(() => {
