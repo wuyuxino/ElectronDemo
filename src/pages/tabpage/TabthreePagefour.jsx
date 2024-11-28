cf49837059ce2e17f873e83dd01c8f24d825163e
wuyx <987012345@qq.com>
2024-11-28 10:31:58 +0800

    fix:hidden bmap

--- src/pages/tabpage/TabthreePagefour.jsx
+++ src/pages/tabpage/TabthreePagefour.jsx
@@ -4,7 +4,8 @@ export default function TabthreePagefour() {
 
     return (
         <div style={{ display: "flex", height: "calc(100% - 10px)" }}>
-            <Echarts4 />
+            {/* <Echarts4 /> */}
+            key暂时不能使用
         </div>
     )
 }
